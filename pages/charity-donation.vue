263b3c9933a52418e15685f2f1a03384abe387ef
DrGo <salah.mahmud@gmail.com>
2022-10-30 14:52:56 -0600

    fund details

--- pages/charity-donation.vue
+++ pages/charity-donation.vue
@@ -6,11 +6,13 @@
     <div class="section details">
       <!-- CONTENT START -->
       <h1 class="title">Charity Donations</h1>
-      <p>Instead of flowers, please consider making a donation to one of the following charities:</p>
-      <h2>Charity 1</h2>
-      <p>something about <em>Charity 1</em>.
+      <p>Instead of flowers, please consider making a donation to :</p>
+      <h2>Dene Ts'ı̨lı̨ - Dene Way of Life Fund</h2>
+      <p>This fund supports qualified donees undertaking Indigenous training and research that promote Dene and Métis
+        environmental governance in the Sahtú Region, Northwest Territories; a cause that Debby dedicated her life for.
       </p>
-      <a class="button" target="_blank" rel=”noopener” href="https://www.justgiving.com/fundraising/">Donate to charity 1</a>    
+      <a class="button" target="_blank" rel=”noopener”
+        href="https://makewaygifts.secure.force.com/donate/?id=a3061000002oeAh">Donate to the Dene Way of Life Fund</a>
       <!-- CONTENT END -->
     </div>
   </div>
@@ -21,4 +23,5 @@ export default {}
 </script>
 
 <style lang="scss">
+
 </style>
