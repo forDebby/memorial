72578097c91f42b3c3e91c8efbc12c770a897686
DrGo <salah.mahmud@gmail.com>
2022-10-29 19:53:50 -0600

    initial commit

---
+++ pages/charity-donation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="section links">
+      <Nav />
+    </div>
+    <div class="section details">
+      <!-- CONTENT START -->
+      <h1 class="title">Charity Donations</h1>
+      <p>Instead of flowers, please consider making a donation to one of the following charities:</p>
+      <h2>Charity 1</h2>
+      <p>something about <em>Charity 1</em>.
+      </p>
+      <a class="button" target="_blank" rel=”noopener” href="https://www.justgiving.com/fundraising/">donate to charity 1</a>
+      <h2>Charity 2</h2>
+      <p><em>Charity 2</em>'s goal of giving joy to children during a terrible time is something that would be a fitting tribute.</p>
+      <a class="button" target="_blank" rel=”noopener” href="https://www.justgiving.com/fundraising/">Donate to Charity 2</a>
+      <!-- CONTENT END -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+</style>
---
+++ pages/live-stream.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="section links">
+      <Nav />
+    </div>
+    <div class="section details">
+      <!-- CONTENT START -->
+      <h1 class="title">Service Livestream</h1>
+      <p>The church service is being performed at Church address
+      </p>
+      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 10:30am</a>
+      <!-- CONTENT END -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+</style>
