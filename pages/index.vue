d77e4bc67b72fd0ac8cbfa429d0cc6c5647ccfcb
DrGo <salah.mahmud@gmail.com>
2022-10-30 14:41:02 -0600

    more pics

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,11 @@
       <img height="292" src="~/assets/frontimage.jpg" alt="In loving memory" />
       <div class="subtitle"><div class="name">{{ $config.name }}</div>{{ $config.dates }}</div>
       <!-- CONTENT END -->
+      <div>
+        <p class="subtitle">&copy; 2022 Salah Mahmud and the Simmons Family</p>
+      </div>
     </div>
+
   </div>
 </template>
 
