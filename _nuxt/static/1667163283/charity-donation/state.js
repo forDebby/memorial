545275e7af461a21221ca4432344667be8c3e221
DrGo <DrGo@users.noreply.github.com>
2022-10-30 20:54:45 +0000

    deploy: 263b3c9933a52418e15685f2f1a03384abe387ef

---
+++ _nuxt/static/1667163283/charity-donation/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmemorial\u002F_nuxt\u002Fstatic\u002F1667163283",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcharity-donation",config:{name:"Deborah Simmons",dates:"October 2 1962 - October 28 2022",_app:{basePath:"\u002Fmemorial\u002F",assetsPath:"\u002Fmemorial\u002F_nuxt\u002F",cdnURL:a}}}}(null));
