13b5c39be374210ab987d5fb02f7624db40cc8c1
DrGo <salah.mahmud@gmail.com>
2022-10-30 12:08:08 -0600

    add obituaries

---
+++ pages/obituaries.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="section links">
+      <Nav />
+    </div>
+    <div class="section details">
+      <!-- CONTENT START -->
+      <h1 class="title">Obituaries</h1>
+      <p>Deborah L Simmons (1962-2022)’s death is such a loss. She grew up in Denendeh (the Northwest Territories) and lived there again for most of the years since 1999. Some of you will know her from her years in Toronto￼ from the late 1980s until 1995, others from her time in Winnipeg (2004-2006). Debbie was an amazing person, as capable in intellectual matters (her regrettably unpublished dissertation on the political economy of Indigenous resistance in Canada is excellent) as she was on the land, befriending people in a way that reflected her wide range of interests, generous, outgoing, energetic.
+She was a friend and comrade￼ of mine since the early 90s in Toronto, and I was so happy when she moved to Winnipeg a year after I did￼. After two years she decided to leave academic work and go back to living in the north, which in spite of her urban cosmopolitan side was, I think, where ￼she was most at home.</p>
+  <h3>David Camfield</h3>
+      <!-- CONTENT END -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+</style>
