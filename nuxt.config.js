0bdf8ce3766ad1efc80397b2fb7fe0a504b20c06
DrGo <salah.mahmud@gmail.com>
2022-10-29 23:37:47 -0600

    change router

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,9 @@ export default {
   },
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
+  router: {
+    base: '/memorial/'
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     htmlAttrs: {
