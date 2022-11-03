f156bdd8ae01b6035b651fb593ce508a4fa9aa91
DrGo <salah.mahmud@gmail.com>
2022-11-03 15:22:52 -0600

    few edits

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -10,7 +10,7 @@
       <p> A memorial service for Deborah Lee Simmons will be held on <em>Saturday, November 12 at 2pm (MT)</em> at the Pincher
         Creek United Church (972 Police Ave, Pincher Creek, Alberta (<a
         href="https://www.google.com/maps/dir//972+Police+Ave,+Pincher+Creek,+AB+T0K+1W0/@49.4847493,-114.0163185,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x536f0a6555e03d5f:0x33f0658439434592!2m2!1d-113.9462789!2d49.4847704">Directions</a>).
-        This will be followed by a gathering at the [TBD].
+        This will be followed by a gathering at the <span>TBD</span>.
       </p>
       <p class="highlight"><a href="https://forms.gle/gXuoDZ95VqBU9J3a8">RSVP</a></p>
 
@@ -18,10 +18,10 @@
 
       <p>
       We will also hold a celebration of Debby's life in the Northwest Territories in the future (details to be
-      determined at a later date.)</p>
+      determined at a later date).</p>
 
 
-      <h2>Church Service <span>TBD</span></h2>
+      <h2>Church Service </h2>
       <ul class="fa-ul">
         <ListItem>Service starts at 2 PM (MT)</ListItem>
         <ListItem>The Pincher Creek United Church. 972 Police Ave, Pincher Creek, AB</ListItem>
