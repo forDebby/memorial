76fb6df7d4ed10bcc131af0aeb9d223f7ed60fd0
DrGo <DrGo@users.noreply.github.com>
2022-11-06 00:10:45 +0000

    deploy: e92e7a08199c2af7e5121c24f870c7b4748017c7

---
+++ _nuxt/static/1667693443/poem/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmemorial\u002F_nuxt\u002Fstatic\u002F1667693443",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpoem",config:{name:"Deborah Simmons",dates:"October 2 1962 - October 28 2022",_app:{basePath:"\u002Fmemorial\u002F",assetsPath:"\u002Fmemorial\u002F_nuxt\u002F",cdnURL:a}}}}(null));
