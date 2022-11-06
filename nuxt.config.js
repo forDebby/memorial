e92e7a08199c2af7e5121c24f870c7b4748017c7
DrGo <salah.mahmud@gmail.com>
2022-11-05 18:06:20 -0600

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
       { hid: 'description', name: 'description', content: 'Find details on the life and legacy of ' + name }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/memorial/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;1,700&family=Pinyon+Script&display=swap' }
     ]
   },
