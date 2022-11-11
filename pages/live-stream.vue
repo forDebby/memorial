2b8428c726d72eab65fad9336fd4392405cbfee1
DrGo <salah.mahmud@gmail.com>
2022-11-11 16:30:35 -0700

    add youtube livestream

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -6,9 +6,9 @@
     <div class="section details">
       <!-- CONTENT START -->
       <h1 class="title">Service Livestream</h1>
-      <p>The church service will be streamed here
-      </p>
-      <a class="button" target="_blank" rel=”noopener” href="">Livestream - pending</a>
+      
+      <iframe width="852" height="479" src="https://www.youtube.com/embed/ga8wEr2tjFc" title="Deborah Simmons Memorial Service" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>      
+      <!-- <a class="button" target="_blank" rel=”noopener” href="">Livestream - pending</a> -->
       <!-- CONTENT END -->
     </div>
   </div>
